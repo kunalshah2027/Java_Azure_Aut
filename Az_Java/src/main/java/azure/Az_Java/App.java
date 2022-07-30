c002cf7b28753b08bfddefa61de37cb513a898b1
kunalshah2027 <kunsh2027@gmail.com>
2022-07-30 17:24:58 +0530

    Test

---
+++ Az_Java/src/main/java/azure/Az_Java/App.java
@@ -0,0 +1,13 @@
+package azure.Az_Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
