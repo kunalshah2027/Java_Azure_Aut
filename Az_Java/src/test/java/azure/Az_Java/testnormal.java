8b2f60a897d190446637fe1ef6b62185021d7d05
kunalshah2027 <kunsh2027@gmail.com>
2022-07-30 18:14:26 +0530

    SampleMavenTestNGProject

---
+++ Az_Java/src/test/java/azure/Az_Java/testnormal.java
@@ -0,0 +1,17 @@
+package azure.Az_Java;
+
+import org.testng.annotations.Test;
+
+public class testnormal {
+	
+	@Test
+	public void add()
+	{
+		int a = 2;
+		int b = 4;
+		int c = a + b;
+		System.out.println("addition of twonumber is 6");
+		System.out.println(c);
+	}
+
+}
